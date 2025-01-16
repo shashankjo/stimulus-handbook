4ca08440e7f95cd0c3d2ad9d7dbc7aaedc169a32
shashankjo <kumarshashank.jo@gmail.com>
2025-01-16 13:37:20 +0530

    Slideshow with basic index updating

---
+++ src/controllers/slideshow_controller.js
@@ -0,0 +1,27 @@
+// src/controllers/slideshow_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "slide" ]
+
+  initialize() {
+    this.index = 0
+    this.showCurrentSlide()
+  }
+
+  next() {
+    this.index++
+    this.showCurrentSlide()
+  }
+
+  previous() {
+    this.index--
+    this.showCurrentSlide()
+  }
+
+  showCurrentSlide() {
+    this.slideTargets.forEach((element, index) => {
+      element.hidden = index !== this.index
+    })
+  }
+}
